cde4f895e0ee6bee3fee52e94f0633ea490bca27
Shun <shun06191999@icloud.com>
2025-07-30 17:34:14 +0900

    google authentication image

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.microcms-assets.io',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
   reactStrictMode: false,
