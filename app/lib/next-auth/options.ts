ed5531eb21c529e5efee89d5aea6be99ccd55874
Shun <shun06191999@icloud.com>
2025-07-30 17:09:02 +0900

    google authentication

--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import prisma from "../prisma";
+import GoogleProvider from "next-auth/providers/google";
 
 export const nextAuthOptions: NextAuthOptions = {
   debug : false,
@@ -10,6 +11,10 @@ export const nextAuthOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  })
   ],
   adapter:PrismaAdapter(prisma),
   callbacks: {
